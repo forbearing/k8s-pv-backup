a554c54857a72492fe9d579dfb7959119368e6bc
forbearing <fredjoseph863@gmail.com>
2021-09-24 17:33:27 +0800

    Universal Backup Script

--- backup-script.sh
+++ backup-script.sh
@@ -38,6 +38,23 @@ cumul_backup_snap_name="${cumul_backup_path}"/"${backup_target}"-"cumul.sngz"
 cumul_backup_file_name="${cumul_backup_path}"/"${backup_target}"-"cumul"-$(date +%d)-$(date +%H%M)".tgz"
 
 
+LOCKFILE="/var/lock/backup-script"
+LOCKFD=99
+ 
+# PRIVATE
+_lock()             { flock -$1 $LOCKFD; }
+_no_more_locking()  { _lock u; _lock xn && rm -f $LOCKFILE; }
+_prepare_locking()  { eval "exec $LOCKFD>\"$LOCKFILE\""; trap _no_more_locking EXIT; }
+ 
+# ON START
+_prepare_locking
+ 
+# PUBLIC
+exlock_now()        { _lock xn; }  # obtain an exclusive lock immediately or fail
+exlock()            { _lock x; }   # obtain an exclusive lock
+shlock()            { _lock s; }   # obtain a shared lock
+unlock()            { _lock u; }   # drop a lock
+
 
 function full_backup {
     echo "========== BEGIN ${BACKUP_TARGET^} Full Backup ==========="
